32af0966536d44114f719e5d97bf3b5438793023
AnastasiyaSv <svitavskaya.anastasiya@yandex.ru>
2023-11-14 11:14:12 +0500

    authorization test for the entire list of users

--- selenium_04/inventory_page.py
+++ selenium_04/inventory_page.py
@@ -1,6 +1,5 @@
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
-from selenium.webdriver import ActionChains, Keys
 from selenium.webdriver.common.by import By
 
 class Inventory_Page:

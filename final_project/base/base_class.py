11cffc7db5680924794c2fdcb4c9cd0b96bebfa8
AnastasiyaSv <svitavskaya.anastasiya@yandex.ru>
2023-12-21 13:49:32 +0500

    add logger

--- final_project/base/base_class.py
+++ final_project/base/base_class.py
@@ -2,5 +2,5 @@ class Base():
     def __init__(self, driver):
         self.driver = driver
 
-    INDILOGIN = 'selen500' # ввести свой или тестовый логин
-    INDIPASS = 'SunRise613' #ввести свой или тестовый пароль
+    INDILOGIN = '***' # ввести свой или тестовый логин
+    INDIPASS = '***' #ввести свой или тестовый пароль
